8251969e832df56db38d5d656222d7e035cfca0f
Martin H. Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2026-03-03 13:18:22 +0100

    Eksempel med unike heltall

--- uke5/test_unique_integers.py
+++ uke5/test_unique_integers.py
@@ -1,5 +1,17 @@
-# import pytest
+import pytest  # pip install pytest
+from unique_integers import UniqueIntegers
 
 
 def test_unique_integers():
-    pass
+    my_ints = UniqueIntegers()
+    my_ints.append(3)
+    my_ints.append(4)
+    my_ints.append(3)
+    assert my_ints.as_list() == [3, 4]
+
+
+def test_unique_int_invalid_input():
+    my_ints = UniqueIntegers()
+    my_ints.append(3)
+    with pytest.raises(TypeError):
+        my_ints.append("sushi")
--- uke5/unique_integers.py
+++ uke5/unique_integers.py
@@ -1,2 +1,12 @@
 class UniqueIntegers:
-    pass
+    def __init__(self):
+        self.list = []
+
+    def append(self, number: int):
+        if not isinstance(number, int):
+            raise (TypeError(f"Input {number} is not an int"))
+        if number not in self.list:
+            self.list.append(number)
+
+    def as_list(self) -> list[int]:
+        return self.list
